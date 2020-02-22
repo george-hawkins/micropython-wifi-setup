3f4b9ffe1a957c4b695b6de8d41ee60213dd6ce6
George Hawkins <george-hawkins@users.noreply.github.com>
2020-02-22 21:20:34 +0100

    Added in microDNSSrv.

--- main.py
+++ main.py
@@ -6,29 +6,23 @@ ap.config(essid="esp32-5cd80b3", authmode=network.AUTH_OPEN)
 
 # ----------------------------------------------------------------------
 
+from microDNSSrv import MicroDNSSrv
+
+addr = ap.ifconfig()[0]
+
+MicroDNSSrv.Create( { "*" : addr } )
+
+# ----------------------------------------------------------------------
+
 from MicroWebSrv2  import *
 from time          import sleep
 
 mws2 = MicroWebSrv2()
 
+# Reduce slot count as memory is at a minimum.
+mws2.BufferSlotsCount = 8
+
 # Redirect all unfound paths to the root.
 mws2.NotFoundURL = '/'
 
 mws2.StartManaged()
-#
-## Main program loop until keyboard interrupt,
-#try :
-#    while mws2.IsRunning :
-#        sleep(1)
-#except KeyboardInterrupt :
-#    pass
-#
-## End,
-#print()
-#mws2.Stop()
-#print('Bye')
-#print()
-#
-## ============================================================================
-## ============================================================================
-## ============================================================================
