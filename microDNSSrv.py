08097e436be1abf6b81c505293eeeabb0a041766
George Hawkins <george-hawkins@users.noreply.github.com>
2020-02-26 13:19:32 +0100

    Slimmed down microDNSSrv to be non-threaded and leave socket polling to the user of microDNSSrv.

--- microDNSSrv.py
+++ microDNSSrv.py
@@ -3,47 +3,13 @@ The MIT License (MIT)
 Copyright © 2018 Jean-Christophe Bos & HC² (www.hc2.fr)
 """
 
-from   _thread import start_new_thread
-from   re      import match
 import socket
-import gc
 
 class MicroDNSSrv :
 
     # ============================================================================
-    # ===( Speed Creation )=======================================================
-    # ============================================================================
-
-    def Create(domainsList) :
-        mds = MicroDNSSrv()
-        if mds.SetDomainsList(domainsList) and mds.Start() :
-            return mds
-        return None
-
-    # ============================================================================
     # ===( Utils )================================================================
     # ============================================================================
-
-    def _tryStartThread(func, args=()) :
-        for x in range(10) :
-            try :
-                gc.collect()
-                start_new_thread(func, args)
-                return True
-            except :
-                global _dns_thread_id
-                try :
-                    _dns_thread_id += 1
-                except :
-                    _dns_thread_id = 0
-                try :
-                    start_new_thread('DNS_THREAD_%s' % _dns_thread_id, func, args)
-                    return True
-                except :
-                    pass
-        return False
-
-    # ----------------------------------------------------------------------------
 
     def _ipV4StrToBytes(ipStr) :
         try :
@@ -117,39 +83,25 @@ class MicroDNSSrv :
     # ============================================================================
 
     def __init__(self) :
-        self._domList = { }
         self._started = False
 
     # ============================================================================
     # ===( Server Thread )========================================================
     # ============================================================================
 
-    def _serverProcess(self) :
-        self._started = True
-        while True :
-            try :
-                packet, cliAddr = self._server.recvfrom(256)
-                domName = MicroDNSSrv._getAskedDomainName(packet)
-                if domName :
-                    domName = domName.lower()
-                    ipB = self._domList.get(domName, None)
-                    if not ipB :
-                        for domChk in self._domList.keys() :
-                            if domChk.find('*') >= 0 :
-                                r = domChk.replace('.', '\.').replace('*', '.*') + '$'
-                                if match(r, domName) :
-                                    ipB = self._domList.get(domChk, None)
-                                    break
-                        if not ipB :
-                            ipB = self._domList.get('*', None)
-                    if ipB :
-                        packet = MicroDNSSrv._getPacketAnswerA(packet, ipB)
-                        if packet :
-                            print('Resolved', domName)
-                            self._server.sendto(packet, cliAddr)
-            except :
-                if not self._started :
-                    break
+    def process_request(self, lookup) :
+        try :
+            packet, cliAddr = self._server.recvfrom(256)
+            domName = MicroDNSSrv._getAskedDomainName(packet)
+            if domName :
+                domName = domName.lower()
+                ipB = lookup(domName)
+                if ipB :
+                    packet = MicroDNSSrv._getPacketAnswerA(packet, ipB)
+                    if packet :
+                        self._server.sendto(packet, cliAddr)
+        except Exception as e:
+            sys.print_exception(e)
 
     # ============================================================================
     # ===( Functions )============================================================
@@ -165,8 +117,7 @@ class MicroDNSSrv :
                                      1 )
             self._server.bind(('0.0.0.0', 53))
             self._server.setblocking(True)
-            return MicroDNSSrv._tryStartThread(self._serverProcess)
-        return False
+            self._started = True
 
     # ----------------------------------------------------------------------------
 
@@ -181,23 +132,6 @@ class MicroDNSSrv :
 
     def IsStarted(self) :
         return self._started
-
-    # ----------------------------------------------------------------------------
-
-    def SetDomainsList(self, domainsList) :
-        if domainsList and isinstance(domainsList, dict) :
-            o = { }
-            for dom, ip in domainsList.items() :
-                if isinstance(dom, str) and len(dom) > 0 :
-                    ipB = MicroDNSSrv._ipV4StrToBytes(ip)
-                    if ipB :
-                        o[dom.lower()] = ipB
-                        continue
-                break
-            if len(o) == len(domainsList) :
-                self._domList = o
-                return True
-        return False
 
     # ============================================================================
     # ============================================================================
