ebccec2c1e1a7c59413fa0c33edc779f83a93d88
George Hawkins <george-hawkins@users.noreply.github.com>
2020-04-05 16:25:28 +0200

    Added ability to compress files in advance and serve them later with content-encoding gzip.

--- slim/fileserver_module.py
+++ slim/fileserver_module.py
@@ -12,12 +12,16 @@ class FileserverModule:
         if request.IsUpgrade or request.Method not in ("GET", "HEAD"):
             return
 
-        filename = self._resolve_physical_path(request.Path)
+        (filename, compressed) = self._resolve_physical_path(request.Path)
         if filename:
             ct = self._get_mime_type_from_filename(filename)
             if ct:
                 request.Response.AllowCaching = True
                 request.Response.ContentType = ct
+
+                if compressed:
+                    request.Response.SetHeader("Content-Encoding", "gzip")
+                    filename = compressed
                 request.Response.ReturnFile(filename)
             else:
                 request.Response.ReturnForbidden()
@@ -26,15 +30,22 @@ class FileserverModule:
 
     def _resolve_physical_path(self, url_path):
         if ".." in url_path:
-            return None  # Don't allow trying to escape the root.
+            return None, None  # Disallow trying to escape the root.
 
         if url_path.endswith("/"):
             url_path = url_path[:-1]
         path = self._root + url_path
+
         if isdir(path):
             path = path + "/" + self._DEFAULT_PAGE
 
-        return path if exists(path) else None
+        if exists(path):
+            return path, None
+
+        compressed = path + ".gz"
+
+        # The tuple parentheses aren't optional here.
+        return (path, compressed) if exists(compressed) else (None, None)
 
     def _get_mime_type_from_filename(self, filename):
         def ext(name):
