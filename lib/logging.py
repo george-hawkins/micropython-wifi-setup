a8e176511e29eb978314bc6c95b8ad4dd2f3e45f
George Hawkins <george-hawkins@users.noreply.github.com>
2020-05-23 12:06:01 +0200

    Minor cleanup of logging.py to use CPython level names and to remove sys.exc_info() (which is not available in MicroPython 1.12).

--- lib/logging.py
+++ lib/logging.py
@@ -8,9 +8,9 @@ DEBUG = 10
 NOTSET = 0
 
 _level_dict = {
-    CRITICAL: "CRIT",
+    CRITICAL: "CRITICAL",
     ERROR: "ERROR",
-    WARNING: "WARN",
+    WARNING: "WARNING",
     INFO: "INFO",
     DEBUG: "DEBUG",
 }
@@ -54,6 +54,8 @@ class Logger:
     def warning(self, msg, *args):
         self.log(WARNING, msg, *args)
 
+    warn = warning
+
     def error(self, msg, *args):
         self.log(ERROR, msg, *args)
 
@@ -64,36 +66,21 @@ class Logger:
         self.log(ERROR, msg, *args)
         sys.print_exception(e, _stream)
 
-    def exception(self, msg, *args):
-        self.exc(sys.exc_info()[1], msg, *args)
-
 
 _level = INFO
 _loggers = {}
 
 
 def getLogger(name):
-    if name in _loggers:
-        return _loggers[name]
-    l = Logger(name)
-    _loggers[name] = l
-    return l
+    if name not in _loggers:
+        _loggers[name] = Logger(name)
+    return _loggers[name]
 
 
-def info(msg, *args):
-    getLogger(None).info(msg, *args)
-
-
-def debug(msg, *args):
-    getLogger(None).debug(msg, *args)
-
-
-def basicConfig(level=INFO, filename=None, stream=None, format=None):
+def basicConfig(level=INFO, filename=None, stream=None):
     global _level, _stream
     _level = level
     if stream:
         _stream = stream
     if filename is not None:
         print("logging.basicConfig: filename arg is not supported")
-    if format is not None:
-        print("logging.basicConfig: format arg is not supported")
